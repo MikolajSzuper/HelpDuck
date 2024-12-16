72f01011ea091e1d5b4ce7d0f3d2bc825121e4c7
Mikołaj Szuper <mikisuper09@gmail.com>
2024-12-16 22:50:41 +0100

    First concept

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help Duck</title>
+    <link rel="stylesheet" href="css/global.css" type="text/css" />
+    <link rel="icon" href="images/kaczk2.png" type="image/x-icon" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">
+                <img src="images/logo.png" alt="Logo">
+            </div>
+            <nav>
+                <a href="#"><span style="color: #ca935e;">Strona Główna</span></a>
+                <a href="#">Graj</a>
+                <a href="#">Pomoc</a>
+                <a href="#">Najlepsi pomocnicy</a>
+                <a href="#">Profil</a>
+            </nav>
+        </header>
+        <div class="main-content">
+                <div class="duck-drawing">
+                    <img src="images/kaczk2.png" alt="Kaczka">
+                </div>
+                <div class="text-content">
+                    <h1><span style="color: #ca935e;">Help</span> Duck</h1>
+                    <div class="content">
+                        <p>Help Duck to gra pełna wyzwań i zabawy! Twoim zadaniem jest pomóc małej, odważnej kaczce przedostać się bezpiecznie przez ruchliwą ulicę. Uważaj na samochody, rowerzystów i inne przeszkody, które mogą stanąć na jej drodze!</p>
+                        <p>Każdy poziom staje się trudniejszy, a ulice bardziej zatłoczone. Zbieraj punkty, omijaj niebezpieczeństwa i doprowadź kaczkę na drugą stronę w jednym kawałku. Gra łączy zręczność z humorem, zapewniając świetną rozrywkę dla graczy w każdym wieku.</p>
+                        <p>Czy uda Ci się przejść wszystkie poziomy? Pomóż kaczce już teraz!</p>
+                    </div>
+                    <div class="buttons">
+                        <button onclick="window.location.href='zaloguj.php'">Zaloguj</button>
+                        <button onclick="window.location.href='zarejestruj.php'">Zarejestruj</button>
+                    </div>
+                </div>
+        </div>
+        <footer>
+            <p>&copy;MS Help Duck</p>
+        </footer>
+    </div>
+</body>
+</html>
